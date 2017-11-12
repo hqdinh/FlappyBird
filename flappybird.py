67ae9baa531e6e1f633816fdfb75f42e37c0630c
Dinh Quang Huy <32260482+hqdinh@users.noreply.github.com>
2017-11-11 16:00:40 -0800

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -1,103 +1,183 @@
-#!/usr/bin/env python
+""" Flappy Bird game in python
+Real gravity physics. 
+All parameters in Config can be changed without breaking the game
+Simplified graphics (no background nor bird sprites)
+Quit by pressing ESCAPE or click the x-mark
+Play by clicking on the window or pressing any other key
+"""
+ 
 
-import pygame
-from pygame.locals import *  # noqa
 import sys
 import random
+import pygame
 
+QUIT = pygame.QUIT
+MOUSECLICK = pygame.MOUSEBUTTONDOWN
+KEYDOWN = pygame.KEYDOWN
+K_ESCAPE = pygame.K_ESCAPE
 
-class FlappyBird:
+class Config:
+    """Defines configurations of the game"""    
     def __init__(self):
-        self.screen = pygame.display.set_mode((400, 708))
-        self.bird = pygame.Rect(65, 50, 50, 50)
-        self.background = pygame.image.load("assets/background.png").convert()
-        self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
-                            pygame.image.load("assets/2.png").convert_alpha(),
-                            pygame.image.load("assets/dead.png")]
-        self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
-        self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
-        self.gap = 130
-        self.wallx = 400
-        self.birdY = 350
-        self.jump = 0
-        self.jumpSpeed = 10
-        self.gravity = 5
+        self.bird_x = 50  # Starting x-coor of bird. Stays the same throughout
+        self.bird_yo = 50  # Starting y-coor of bird
+        self.gravity = 0.8  # How fast bird accel. downward
+        self.jump_v = 10  # Each jump will set the bird's velocity to jump_v
+        self.gap = 200  # Gap between each set of upper and lower pipes
+        self.pipe_speed = 2  # How fast the pipes move to the left
+        self.pipe_dist = 200  # How far each set of pipes are (the smaller the harder)
+        self.offs = 200  # how far each subsequent gap may vary vertically 
+        self.screen_W = 400  # screen width
+        self.screen_H = 700  # screen height
+        
+class FlappyBird:
+    """Game class"""    
+    
+    def __init__(self):
+        # Load configurations from Config
+        self.config = Config()
+        self.bird_x = self.config.bird_x
+        self.bird_yo = self.config.bird_yo
+        self.gravity = self.config.gravity
+        self.jump_v = -self.config.jump_v
+        self.gap = self.config.gap
+        self.pipe_speed = self.config.pipe_speed
+        self.pipe_dist = self.config.pipe_dist
+        self.offs = self.config.offs 
+        self.screen_W = self.config.screen_W
+        self.screen_H = self.config.screen_H
+                
+        # Image loading procedures 
+        self.screen = pygame.display.set_mode((self.screen_W, self.screen_H))
+        self.bird = pygame.Rect(self.bird_x, self.bird_yo, 40, 40)
+        self.bird_img = pygame.image.load("assets/pusheen_50.png").convert_alpha()
+        self.pipe_bot = pygame.image.load("assets/bottom.png").convert_alpha()
+        self.pipe_top = pygame.image.load("assets/top.png").convert_alpha()
+        self.pipe_W = self.pipe_top.get_width()
+        self.pipe_H = self.pipe_top.get_height()
+        self.pipe_num = self.screen_W//self.pipe_W
+        
+        # Initiate game variables        
+        self.new_round_handler()
+
+    def rand_offs(self):
+        # Returns distance from midscreen to midpoint of the gap of each pipe pair
+        return random.randint(-self.offs, self.offs)
+    
+    def new_round_handler(self):
+        """Reset these variables at start/death"""      
+        # Pipe settings
+        self.pipe_x = [self.screen_W + n*self.pipe_dist 
+                       for n in range(self.pipe_num)]
+        self.pipe_bot_y = [self.screen_H/2 + self.gap/2 + self.rand_offs() 
+                           for n in range(self.pipe_num)]
+        self.pipe_top_y = [d - self.gap - self.pipe_H 
+                           for d in self.pipe_bot_y]
+        self.pipe_pos = [self.pipe_x, self.pipe_bot_y, self.pipe_top_y]
+        self.pipe_it = 0  # Iterator
+                
+        # Bird settings
+        self.bird_y = self.bird_yo
+        self.bird_vel = 0        
+        self.jump = False # this switches to True when anykey is pressed
         self.dead = False
-        self.sprite = 0
-        self.counter = 0
-        self.offset = random.randint(-110, 110)
+        self.score = 0
+        
+    def update_pipes(self):
+        """Update pipes' positions"""
+        self.pipe_pos[0] = [x - self.pipe_speed for x in self.pipe_pos[0]]
+        self.pipe_it = self.pipe_it % self.pipe_num
+        # replace with new pipe after curr pipe passed left edge of screen
+        if self.pipe_pos[0][self.pipe_it] < -self.pipe_W:
+            # x coordinates of each pair
+            self.pipe_pos[0][self.pipe_it] = self.pipe_pos[0][self.pipe_it] + \
+                                             self.pipe_num*self.pipe_dist
+            # y coordinate of bottom pipes
+            self.pipe_pos[1][self.pipe_it] = self.screen_H/2 + self.gap/2 + \
+                                             self.rand_offs()
+            # y coordinate of top pipes
+            self.pipe_pos[2][self.pipe_it] = self.pipe_pos[1][self.pipe_it] - \
+                                             self.gap - self.pipe_H 
+            self.pipe_it += 1
+            self.score += 1
 
-    def updateWalls(self):
-        self.wallx -= 2
-        if self.wallx < -80:
-            self.wallx = 400
-            self.counter += 1
-            self.offset = random.randint(-110, 110)
-
-    def birdUpdate(self):
+    def update_bird(self):
+        """Update bird's position"""
         if self.jump:
-            self.jumpSpeed -= 1
-            self.birdY -= self.jumpSpeed
-            self.jump -= 1
+            self.bird_vel = self.jump_v
+            self.jump = False
         else:
-            self.birdY += self.gravity
-            self.gravity += 0.2
-        self.bird[1] = self.birdY
-        upRect = pygame.Rect(self.wallx,
-                             360 + self.gap - self.offset + 10,
-                             self.wallUp.get_width() - 10,
-                             self.wallUp.get_height())
-        downRect = pygame.Rect(self.wallx,
-                               0 - self.gap - self.offset - 10,
-                               self.wallDown.get_width() - 10,
-                               self.wallDown.get_height())
-        if upRect.colliderect(self.bird):
+            self.bird_vel = self.bird_vel + self.gravity
+        self.bird_y = self.bird_y + self.bird_vel            
+        self.bird[1] = self.bird_y  # Update bird rectangle's y-coor
+        
+    def collision_check(self):
+        """ Checks for bird-pipe and bird-ground collisions """        
+        
+        # pipe_rects contains all pipe rectangles for collision check
+        pipe_rects = [pygame.Rect(self.pipe_pos[0][n], 
+                                 self.pipe_pos[1][n],
+                                 self.pipe_W - 10,
+                                 self.pipe_H) for n in range(self.pipe_num)]
+        pipe_rects += [pygame.Rect(self.pipe_pos[0][n],
+                                 self.pipe_pos[2][n],
+                                 self.pipe_W - 10,
+                                 self.pipe_H) for n in range(self.pipe_num)]            
+        
+        # checks for visible collision
+        for pipe in pipe_rects:
+            if pipe.colliderect(self.bird):
+                self.dead = True
+        
+        # checks for out of screen collision or hitting the ground
+        if (self.bird[1] > self.screen_H or 
+                (self.bird[1] < 0 and 
+                 self.bird[0] > self.pipe_pos[0][self.pipe_it])):
             self.dead = True
-        if downRect.colliderect(self.bird):
-            self.dead = True
-        if not 0 < self.bird[1] < 720:
-            self.bird[1] = 50
-            self.birdY = 50
-            self.dead = False
-            self.counter = 0
-            self.wallx = 400
-            self.offset = random.randint(-110, 110)
-            self.gravity = 5
+    
+    def draw_objects(self):
+        self.screen.fill((0, 0, 0))
+        for i in range(self.pipe_num):
+                self.screen.blit(self.pipe_bot, (self.pipe_pos[0][i], self.pipe_pos[1][i]))
+                self.screen.blit(self.pipe_top, (self.pipe_pos[0][i], self.pipe_pos[2][i]))
+            
+        self.screen.blit(self.font.render(str(self.score),
+                                     -1, 
+                                     (255, 255, 255)),
+                         (50,50))
+        self.screen.blit(self.bird_img, (self.bird_x, self.bird_y))
 
-    def run(self):
+    def run_instance(self):
+        """Main game"""        
         clock = pygame.time.Clock()
-        pygame.font.init()
-        font = pygame.font.SysFont("Arial", 50)
+        pygame.font.init()        
+        self.font = pygame.font.SysFont("Times New Roman", 50)
+        
         while True:
             clock.tick(60)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            
+            events = pygame.event.get()
+            for event in events:
+                if ((event.type == KEYDOWN and event.key == K_ESCAPE) 
+                                               or event.type == QUIT):
+                    pygame.quit()
                     sys.exit()
-                if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
-                    self.jump = 17
-                    self.gravity = 5
-                    self.jumpSpeed = 10
-
-            self.screen.fill((255, 255, 255))
-            self.screen.blit(self.background, (0, 0))
-            self.screen.blit(self.wallUp,
-                             (self.wallx, 360 + self.gap - self.offset))
-            self.screen.blit(self.wallDown,
-                             (self.wallx, 0 - self.gap - self.offset))
-            self.screen.blit(font.render(str(self.counter),
-                                         -1,
-                                         (255, 255, 255)),
-                             (200, 50))
+                if (event.type == KEYDOWN or event.type == MOUSECLICK):
+                    self.jump = True
+            
+            self.draw_objects()
+            self.update_bird()
+            self.update_pipes()
+            self.collision_check()
             if self.dead:
-                self.sprite = 2
-            elif self.jump:
-                self.sprite = 1
-            self.screen.blit(self.birdSprites[self.sprite], (70, self.birdY))
-            if not self.dead:
-                self.sprite = 0
-            self.updateWalls()
-            self.birdUpdate()
+                self.new_round_handler()
+                
             pygame.display.update()
-
+    
+    @staticmethod
+    def start():
+        game = FlappyBird()
+        game.run_instance()
+        
 if __name__ == "__main__":
-    FlappyBird().run()
+    FlappyBird.start()       
